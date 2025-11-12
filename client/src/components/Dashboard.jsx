967c23b32d049a22139ab4659278ec013bd25b63
hngo11 <hngo11@gmail.com>
2025-11-11 21:45:04 -0500

    Added filtering and sort. Still needs work

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -18,7 +18,12 @@ function Dashboard() {
     const [firstLoading, setFirstLoading] = useState(true)
     const [secondLoading, setSecondLoading] = useState(true)
     const [filteredList, setFilteredList] = useState([]);
+    const [sortedList, setSortedList] = useState([]);
+    const [tableList, setTableList] = useState([]);
+    const [assignedFilter, setAssignedFilter] = useState("");
+    const [statusFilter, setStatusFilter] = useState("");
     const [show, setShow] = useState(false);
+    const [sort, setSort] = useState("")
 
     const handleClose = () => setShow(false);
     const toggleShow = () => setShow((s) => !s);
@@ -59,11 +64,8 @@ function Dashboard() {
                 const response = await fetch(ticketsURL)
                 const data = await response.json()
                 setTickets(data)
-                
-                const filter = data.filter(item =>
-                item.assigned.toLowerCase().includes(user.email.toLowerCase()))
-                console.log(filter)
-                setFilteredList(filter)
+                setFilteredList(data)
+                setTableList(data)
             }
             catch(err){console.log(err)}
             finally{
@@ -71,6 +73,7 @@ function Dashboard() {
             }
         }
         getTickets()
+
         
     },[firstLoading])    
 
@@ -81,6 +84,56 @@ function Dashboard() {
         console.log(tickets)
         console.log(filteredList)
     },[secondLoading])
+
+    const onFilter = async (assigned, status) => {
+        
+        let filter = tickets
+
+        if (assigned !== '') {
+            filter = filter.filter(item =>
+                item.assigned.toLowerCase().includes(assigned.toLowerCase()));
+        }
+         if (status !== '') {
+            filter = filter.filter(item =>
+                item.status.toLowerCase().includes(status.toLowerCase()));
+        }
+
+        handleClose()
+        await setAssignedFilter(assigned)
+        await setStatusFilter(status)
+        await setFilteredList(filter)
+        setTableList(filter)
+
+    }
+
+    const handleSort = async (event) => {
+        
+        const selection = event.target.value
+        await setSort(selection)
+        await sortTable(selection)
+    }
+
+    const sortTable = async (selection) => {
+
+         let list = filteredList
+         console.log(list)
+
+        switch (selection) {
+            case 'Title':
+            case 'Assigned':
+            case 'Status':
+                const sortedData = list.sort((a, b) => {
+                    const valA = a[selection.toLowerCase()];
+                    const valB = b[selection.toLowerCase()];
+                    return valA.localeCompare(valB)
+                })
+                return setTableList(sortedData);  
+            case 'Date(Newest)':
+            case 'Date(Oldest)':
+            default:
+                return setTableList(list);
+        }
+    }
 
 
     return (<>
@@ -98,37 +151,58 @@ function Dashboard() {
                         <h1>{user.username}'s Dashboard</h1>
                     </div>
                     <Button variant="primary" onClick={toggleShow} className="me-2">
-                        Filter (In Dev.)
+                        Filter List
                     </Button>
                     <Offcanvas show={show} onHide={handleClose} scroll={true} backdrop={false}>
                         <Offcanvas.Header closeButton>
-                        <Offcanvas.Title>Filter Table</Offcanvas.Title>
+                            <Offcanvas.Title>Filter Table</Offcanvas.Title>
                         </Offcanvas.Header>
-                        <Form>
-                            <Form.Group className=" w-50" controlId="name">
-                                <Form.Label> Currently Assigned:</Form.Label>
-                                <Form.Control
-                                    type="text" 
-                                    placeholder=""/>  
-                            </Form.Group>
-                            <Form.Group className=" w-50" controlId="name">
-                                    <Form.Label>Ticket Status</Form.Label>
-                                <Form.Select
-
-                                    aria-label="Default select example">
-                                    
-                                    <option></option> 
-                                    <option>New</option> 
-                                    <option>In Progress</option> 
-                                    <option>Resolved</option> 
-                                </Form.Select>
-                            </Form.Group>  
-                            <br/>            
-                            <Button variant="primary" onClick={handleClose}  className="me-2">
-                                Update Search
-                            </Button>
-                        </Form>
+                        <Offcanvas.Body>
+                            <Form>
+                                <Form.Group className=" w-50" controlId="Assigned">
+                                    <Form.Label> Currently Assigned:</Form.Label>
+                                    <Form.Control
+                                        type="text" 
+                                        placeholder=""
+                                        value={assignedFilter}
+                                        onChange={(e)=>setAssignedFilter(e.target.value)}/>  
+                                </Form.Group>
+                                <Form.Group className=" w-50" controlId="Status">
+                                        <Form.Label>Ticket Status</Form.Label>
+                                    <Form.Select
+                                        value={statusFilter}
+                                        onChange={(e)=>setStatusFilter(e.target.value)}
+                                        aria-label="Default select example">
+                                        <option></option> 
+                                        <option>New</option> 
+                                        <option>In Progress</option> 
+                                        <option>Resolved</option> 
+                                    </Form.Select>
+                                </Form.Group>  
+                                <br/>            
+                                <Button variant="primary" onClick={() => onFilter(assignedFilter, statusFilter)}  className="me-2">
+                                    Update Search
+                                </Button>
+                                 <Button variant="tertiary" onClick={() => onFilter("","")}  className="me-2">
+                                    Clear Filters
+                                </Button>
+                            </Form>
+                        </Offcanvas.Body>
                     </Offcanvas>
+                     <Form.Group className=" w-25" controlId="Status">
+                                        <Form.Label>Sort By</Form.Label>
+                                    <Form.Select
+                                        value={sort}
+                                        onChange={handleSort}
+                                        aria-label="Default select example">
+                                        <option></option> 
+                                        <option>Title</option> 
+                                        <option>Date(Newest)</option> 
+                                        <option>Date(Oldest)</option>
+                                        <option>Assigned</option>
+                                        <option>Status</option>
+                                    </Form.Select>
+                                </Form.Group>  
                     <Form onSubmit={onSubmit}> 
                         <Container className="d-flex mb-3 justify-content-end">
                             <Button className="d-flex mt-3 mx-3 " variant="primary" type="button" onClick={onSubmit}>
@@ -152,7 +226,7 @@ function Dashboard() {
                             </tr>
                         </thead>
                         <tbody>
-                            {tickets.map((ticket) => (
+                            {tableList.map((ticket) => (
                                 <tr key={ticket._id} onClick={()=>navigate(`/View/${ticket._id}`)}>
                                     <td className="w-25">{ticket.title}</td>
                                     <td className="w-25">{ticket.description}</td>
@@ -163,33 +237,8 @@ function Dashboard() {
                             ))}
                         </tbody>
                     </Table>
-                     </div>
-                    <br/><br/><br/>
-                    {/* <div className="my-1">
-                        <h2>My Tickets</h2>
                     </div>
-                    <Table responsive="sm" className="border border-3">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Creation Date</th>
-                                <th>Assigned</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {filteredList.map((ticket) => (
-                                <tr key={ticket._id}>
-                                    <td className="w-25">{ticket.title}</td>
-                                    <td className="w-25">{ticket.description}</td>
-                                    <td>{ticket.date}</td>
-                                    <td>{ticket.assigned}</td>
-                                    <td>{ticket.status}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table> */}
+                    <br/><br/><br/>
                 </Container>
                 <br/><br/><br/><br/><br/>
                 <Footer/>
